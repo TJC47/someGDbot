b3f7278f1891b8b08dd6e265d4e4ec592bc4bd4c
TJC472 <124094785+TJC47@users.noreply.github.com>
2023-08-14 15:12:04 +0200

    Fix crashes and other issues

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import random
 from threading import Thread
 ignored =[]
+icnf = False
 ops = ["tjc472"]
 #Loading config
 f = open("config.json", "r")
@@ -58,14 +59,14 @@ while botgui.running:
             lastsaid = text
         url = 'http://localhost/postComment'
         while botgui.running:
-            comments=requests.get("http://localhost/api/comments/"+levelID+"?count=3") # annoying issue, please dont make this very high.
+            comments=requests.get("http://localhost/api/comments/"+levelID+"?count="+str(commentfetch)) # annoying issue, please dont make this very high.
             comments=json.loads((comments.text))
             jason["banned"] = ignored
             jason["ops"] = ops
             fi = open("config.json","w")
             fi.write(str(jason).replace("'",'"'))
             fi.close()
-            for comment in range(0,3): #adjust this or it will crash
+            for comment in range(0,commentfetch): #adjust this or it will crash
                 comment1=dict(comments[comment])["content"]
                 comment2=dict(comments[comment])
                 #Debug testing
@@ -73,6 +74,7 @@ while botgui.running:
                     try:
                         comment1 = input("[Debug] Message > ")
                         comment2["ID"] = random.randint(1,100000)
+                        comment2["username"] = "CONSOLE"
                     except:
                         debug()
                 if not comment2["ID"] in ids:
@@ -83,56 +85,61 @@ while botgui.running:
                         file.write("\n"+comment2["username"]+": "+comment1)
                         file.close()
                         arguments = comment1.split(' ', 1)
-                        if "/help" in comment1.lower() and not comment2["username"]==usrname:
-                            if comment2["username"].lower in ops:
+                        icnf = False
+                        if comment1.lower().startswith("/help") and not comment2["username"]==usrname:
+                            if comment2["username"].lower() in ops:
                                 say("@"+comment2["username"]+" OPCommands: /furry /ai /cool /yesorno /say /op /oplist /pen_s /banlist /ban /stats /help")
+                                icnf = True
                             else:
                                 say("@"+comment2["username"]+" Commands: /furry /ai /cool /yesorno /say /oplist /banlist /stats /help")
-                            print("command /help")
+                                icnf = True
                         if comment2["username"].lower() in ops and not comment2["username"]==usrname:
-                            if "/op" in comment1:
+                            if comment1.lower().startswith("/op "):
                                 say("["+comment2["username"]+" opped "+comment1.split(' ', 1)[1]+"]")
                                 ops.append(comment1.split(' ', 1)[1].lower())
-                            if "/ban" in comment1:
+                                icnf = True
+                            if comment1.lower().startswith("/ban "):
                                 say("Banned "+"@"+comment1.split(' ', 1)[1])
                                 ignored.append(comment1.split(' ', 1)[1].lower())
-                            if "/pen_s" in comment1 and not comment2["username"]==usrname:
+                                icnf = True
+                            if comment1.lower().startswith("/pen_s") and not comment2["username"]==usrname:
                                 say("@"+comment2["username"]+" B==D")
-                        elif "/furry" in comment1 and not comment2["username"]==usrname:
+                                icnf = True
+                        if comment1.lower().startswith("/furry") and not comment2["username"]==usrname:
                             say("@"+comment2["username"]+" you are "+str(random.randint(0,100))+"% a furry.")
-                        elif "/banlist" in comment1 and not comment2["username"]==usrname:
+                        elif comment1.lower().startswith("/banlist") and not comment2["username"]==usrname:
                             say("@"+comment2["username"]+" BANNED:"+str(ignored))
-                        elif "/cool" in comment1 and not comment2["username"]==usrname:
+                        elif comment1.lower().startswith("/cool") and not comment2["username"]==usrname:
                             say("@"+comment2["username"]+" you are "+str(random.randint(0,100))+"% cool.")
-                        elif "/yesorno" in comment1.lower() and not comment2["username"]==usrname:
+                        elif comment1.lower().startswith("/yesorno") and not comment2["username"]==usrname:
                             random.seed(comment1.lower())
                             say("@"+comment2["username"]+" "+random.choice(["yes","no"]))
                             random.seed()
-                        elif "/ai" in comment1 and not comment2["username"]==usrname:
-                            resp = requests.get("http://api.brainshop.ai/get?bid=172425&key=f1KfI93QOBQaqZQb&uid=0&msg="+comment1.split(' ', 1)[1], headers={'Accept': 'application/json'})
+                        elif comment1.lower().startswith("/ai") and not comment2["username"]==usrname:
+                            resp = requests.get("http://api.brainshop.ai/get?bid=172425&key=f1KfI93QOBQaqZQb&uid=someone&msg="+comment1.split(' ', 1)[1], headers={'Accept': 'application/json'})
                             jsonResp = json.loads(resp.text)
                             response = jsonResp["cnt"]
                             say("@"+comment2["username"]+" "+response)
-                        elif "/say" in comment1 and not comment2["username"]==usrname:
+                        elif comment1.lower().startswith("/say") and not comment2["username"]==usrname:
                             say("@"+comment2["username"]+" "+comment1.split(' ', 1)[1])
-                        elif "/stats" in comment1 and not comment2["username"]==usrname:
+                        elif comment1.lower().startswith("/stats") and not comment2["username"]==usrname:
                             stats = json.loads(requests.get("http://localhost/api/profile/" + comment2["username"]).text)
                             say("@"+comment2["username"]+ f"  has {stats['stars']} stars, {stats['diamonds']} diamonds, {stats['coins']} coins, {stats['userCoins']} user coins, {stats['demons']} demons beaten and {stats['cp']} creator points.")
-                        elif "/oplist" in comment1 and not comment2["username"]==usrname:
+                        elif comment1.lower().startswith("/oplist") and not comment2["username"]==usrname:
                             say("@"+comment2["username"]+" OPS:"+str(ops))
-                        elif "gg" in comment1.lower() and not comment2["username"]==usrname:
-                            say("@"+comment2["username"]+" Good Game!")
-                        elif "/" in comment1 and not comment2["username"]==usrname:
-                            if comment2["username"].lower in ops:
+                        elif "hello" in comment1.lower() and not comment2["username"]==usrname:
+                            say("@"+comment2["username"]+" Hello!")
+                        elif comment1.startswith("/") and not icnf and not comment2["username"]==usrname:
+                            if comment2["username"].lower() in ops:
                                 say("@"+comment2["username"]+" you have op but this command doesn't exist /help for a list of commands")
                             else:
-                                pass#say("@"+comment2["username"]+" im sorry but you don't have op /help for a list of commands") avoids spamming chat if possible making a cooldown for this message (see todo list) would be the right way 
+                                say("@"+comment2["username"]+" im sorry but you don't have op /help for a list of commands") #avoids spamming chat if possible making a cooldown for this message (see todo list) would be the right way 
                         botgui.comment = comment2["username"]+ ": " + comment1
                         botgui.lastsaid = lastsaid
-                        if alreadyrun == False:
-                            alreadyrun = True
-                            guiwin=Thread(target=botgui.start)
-                            guiwin.start()
+                        #if alreadyrun == False:
+                        #    alreadyrun = True
+                        #    guiwin=Thread(target=botgui.start)
+                        #    guiwin.start()
             if not debugon.lower() ==  "true":
                 time.sleep(2) # Make the bot check every 2 seconds instead, You shouldnt be ratelimited.
     except:
